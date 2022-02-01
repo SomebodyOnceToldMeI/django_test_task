22a6b32b0d7a24a782a3077863f9d366473784ea
Cawoon <yurykavun@gmail.com>
2022-02-01 10:23:16 +0200

    Add views for Post and Like

--- test_task/social_network/views.py
+++ test_task/social_network/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from test_task.social_network.models import Post,Like
+from test_task.social_network.serializers import PostSerializer, LikeSerializer
 # Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('publication_date')
+    serializer_class = PostSerializer
+
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
