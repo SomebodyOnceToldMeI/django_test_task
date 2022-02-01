65a700ba712cdbe8196850f8fb1d7b061924f1ca
Cawoon <yurykavun@gmail.com>
2022-02-01 17:56:14 +0200

    Finalize bot
    Fix typos in urls
    Fix invalid field names

--- bot/bot.py
+++ bot/bot.py
@@ -8,17 +8,20 @@ def infinite_file_generator(filename):
     while True:
         try:
             yield f.__next__().strip()
+        except GeneratorExit:
+            return
         except:
             f.seek(0)
             continue
 
 
+
 def usernames_generator():
-    yield from infinite_file_generator('random_usernames.txt')
+    return infinite_file_generator('random_usernames.txt')
 
 
 def post_messages_generator():
-    yield from infinite_file_generator('random_post_messages.txt')
+    return infinite_file_generator('random_post_messages.txt')
 
 
 class Bot:
@@ -30,15 +33,11 @@ class Bot:
         self.api_operations = api_operations
 
     def signup_users(self, number_of_users):
-        signed_up_users = 0
-        for username in self.usernames_generator:
-
-            if signed_up_users == number_of_users:
-                break
+        for user_index in range(number_of_users):
+            username = self.usernames_generator.__next__()
 
             user = self._signup_user(username)
             self._save_user(user)
-            signed_up_users += 1
 
     def _signup_user(self, username):
         user = self.api_operations.signup_user(username, username)
@@ -80,6 +79,9 @@ class Bot:
                 random_post = random.choice(self.posts)
                 self._like_post(user, random_post)
 
+    def _like_post(self, user, post):
+        self.api_operations.like_post(user, post)
+
 class ApiOperations:
     def __init__(self, api_url):
         self.api_url = api_url
@@ -91,25 +93,30 @@ class ApiOperations:
 
     def create_post(self, user, post_message):
         payload = {'post_text' : post_message}
-        headers = {'Authorization' : user.get('access_token')}
+        headers = {'Authorization' : 'Bearer ' + user.get('access')}
         response = requests.post(self.api_url + '/post/', json = payload, headers = headers)
         response = json.loads(response.text)
-        return {'post_id' : response['post_id']}
+        return {'id' : response['id']}
 
     def authenticate_user(self, user):
         payload = {'username': user['username'], 'password': user['password']}
         response = requests.post(self.api_url + '/login/', json=payload)
         response = json.loads(response.text)
-        user['access_token'] = response['access_token']
+        user['access'] = response['access']
         return user
 
     def like_post(self, user, post):
-        headers = {'Authorization': user.get('access_token')}
-        response = requests.post(self.api_url + '/post/{}/like'.format(post['id']), headers=headers)
+        headers = {'Authorization' : 'Bearer ' + user.get('access')}
+        response = requests.post(self.api_url + '/post/{}/like/'.format(post['id']), headers=headers)
 
-
-#bot = Bot()
-#bot.signup_users(NUMBER_OF_USERS)
-#bot.authenticate_users()
-#bot.create_random_number_of_posts_per_user(MAX_POSTS_PER_USER)
-#bot.randomly_like_posts(MAX_LIKES_PER_USER)
+api_operations = ApiOperations('http://127.0.0.1:8000')
+bot = Bot(api_operations)
+print('Signing up users..')
+bot.signup_users(NUMBER_OF_USERS)
+print('Authenticating users..')
+bot.authenticate_users()
+print("Creating posts..")
+bot.create_random_number_of_posts_per_user(MAX_POSTS_PER_USER)
+print('Liking posts..')
+bot.randomly_like_posts(MAX_LIKES_PER_USER)
+print('Done!')
