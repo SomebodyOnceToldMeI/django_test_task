c221501b0095e513d8213ef5a90abcebcb55a2e2
Cawoon <yurykavun@gmail.com>
2022-02-01 15:03:01 +0200

    Add like and unlike actions to PostViewSet

--- social_network/views.py
+++ social_network/views.py
@@ -3,13 +3,42 @@ from rest_framework import viewsets
 from social_network.models import Post,Like
 from social_network.serializers import PostSerializer, LikeSerializer, UserSerializer
 from django.contrib.auth.models import User
+from rest_framework.decorators import action
 from rest_framework import permissions
+from django.utils import timezone
+from rest_framework.response import Response
+from social_network.permissions import UserPermission
 
-# Create your views here.
+
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    @action(detail=True, methods=['post'])
+    def like(self, request, pk=None):
+        post = Post.objects.get(id=pk)
+        user = request.user
+        try:
+            old_like = Like.objects.get(post=post,liker=user)
+            return Response({'response': "post is already liked before"})
+        except:
+            new_like = Like(post = post, liker = user, like_date = timezone.now())
+            new_like.save()
+            return Response({'response' : "successfully liked"})
+
+    @action(detail=True, methods = ['post'])
+    def unlike(self, request, pk=None):
+        post = Post.objects.get(id=pk)
+        user = request.user
+        try:
+            old_like = Like.objects.get(post=post, liker=user)
+            old_like.delete()
+            return Response({'response': "post is successfully unliked"})
+        except:
+            return Response({'response': "post was not liked before"})
+
+
 
 class LikeViewSet(viewsets.ModelViewSet):
     queryset = Like.objects.all()
