33998c116a300a08a6963c48a3ad75efdf7945f0
Cawoon <yurykavun@gmail.com>
2022-02-01 10:51:09 +0200

    Create valid django app social network

--- social_network/serializers.py
+++ social_network/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from test_task.social_network.models import Post, Like
+from social_network.models import Post, Like
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -11,3 +11,4 @@ class LikeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Like
         fields = ['post', 'liker', 'like_date']
+        
--- test_task/urls.py
+++ test_task/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+from social_network import views
+from django.urls import include, path
+
+router = routers.DefaultRouter()
+router.register(r'posts', views.PostViewSet)
+router.register(r'likes', views.LikeViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
