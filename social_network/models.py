ca0a1b8733a24d60e2992a0745b6fa82af36f7dd
Cawoon <yurykavun@gmail.com>
2022-02-02 16:10:43 +0200

    Add auto_now=True to DateTimeField constructor to automatically set publication_date to now

--- social_network/models.py
+++ social_network/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
     post_text = models.CharField(max_length=5000)
-    publication_date = models.DateTimeField('date published')
+    publication_date = models.DateTimeField('date published', auto_now=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Like(models.Model):
